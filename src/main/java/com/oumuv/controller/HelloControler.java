e17bf81952e1776409a8964852ae67de04b93d66
Oumuv <802711luo>
2018-03-15 14:44:00 +0800

    2018.1.16

--- src/main/java/com/oumuv/controller/HelloControler.java
+++ src/main/java/com/oumuv/controller/HelloControler.java
@@ -1,5 +1,6 @@
 package com.oumuv.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.oumuv.server.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloControler {
-
     @Autowired
     HelloService helloService;
     @RequestMapping(value = "/hi")
     public String hi(@RequestParam String name){
         return helloService.hiService(name);
     }
-
-
 }
